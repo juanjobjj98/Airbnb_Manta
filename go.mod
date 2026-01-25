04cbd9075fb414bd924b452f224fb569b695b96d
juanjobjj98 <juanjobjj98@gmail.com>
2026-01-25 14:21:24 -0500

    Estructura inicial: Carpetas de paquetes y main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JJ/Airbnb_Manta
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("¡Todo listo, JJ! El motor de Go está rugiendo.")
+	fmt.Println("Ubicación: C:\\Users\\JJ\\Desktop\\PROYECTOS GO\\Airbnb_Manta")
+	fmt.Println("--------------------------------------------------")
+}
